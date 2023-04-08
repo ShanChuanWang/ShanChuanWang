b96632c887ead4fea1243b99048b9493a4fd00fc
Shuai Wang <wang0878@algonquinlive.com>
2023-04-08 16:09:01 -0400

    V1

--- app/src/main/java/algonquin/cst2335_yikaiwang_finalproject/MainActivity.java
+++ app/src/main/java/algonquin/cst2335_yikaiwang_finalproject/MainActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //todo
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
